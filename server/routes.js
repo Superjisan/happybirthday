b1add09bfe209e8bd976d44ca79d5d5baa592986
Jisan Zaman <jzaman@wesleyan.edu>
2014-12-25 11:19:00 -0500

    set up proper file structure

---
+++ server/routes.js
@@ -0,0 +1,14 @@
+var index = require('./controllers/index.js');
+
+module.exports = function(app) {
+    // REST conventions
+    //   GET     /resources          ->  index
+    //   GET     /resources/new      ->  new
+    //   POST    /resources          ->  create
+    //   GET     /resources/:id      ->  show
+    //   GET     /resources/:id/edit ->  edit
+    //   PUT     /resources/:id      ->  update
+    //   DELETE  /resources/:id      ->  destroy
+
+    app.use('/', index);
+};
