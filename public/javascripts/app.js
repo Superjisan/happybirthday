b41d06769ac2f5db874de6023dc67f4f520f22cf
Jisan Zaman <jzaman@wesleyan.edu>
2014-12-25 16:47:04 -0500

    jade layout finished

---
+++ public/javascripts/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('happyBirthdayApp', [
+    'ngCookies',
+    'ngResource',
+    'ui.router',
+    'happyBirthdayApp.system'
+])
+
+window.angular.module('happyBirthdayApp.system', []);
