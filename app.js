ad75385ee08b7a847ed117d94bc25c9653fb78ad
Jisan Zaman <jzthegreat@gmail.com>
2015-06-22 13:14:16 -0400

    took out node-sass dependencies

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var sass = require('node-sass');
-var sassMiddleware = require('node-sass-middleware');
 
 
 var app = express();
@@ -20,7 +18,7 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-    extended: false
+  extended: false
 }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -28,9 +26,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handlers
@@ -38,38 +36,29 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
-//sass configuration
-app.use(
-    sassMiddleware({
-        src: __dirname + '/public/stylesheets/sass', //where the sass files are
-        dest: __dirname + '/public/stylesheets', //where css should go
-        debug: true // obvious,
-
-    })
-);
 
 app.set('port', process.env.PORT || 3000);
-// app.listen(app.get('port'), function() {
-//   console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
-// });
+app.listen(app.get('port'), function() {
+  console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
+});
 
 module.exports = app;
