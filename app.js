67e1424c0767cfcefb6af442c5b70080d63a2263
Jisan Zaman <jzthegreat@gmail.com>
2015-06-22 13:25:40 -0400

    only run server in local

--- app.js
+++ app.js
@@ -57,8 +57,12 @@ app.use(function(err, req, res, next) {
 
 
 app.set('port', process.env.PORT || 3000);
-app.listen(app.get('port'), function() {
-  console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
-});
+
+if(app.get('env') === 'development'){
+  app.listen(app.get('port'), function() {
+    console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
+  });
+    
+}
 
 module.exports = app;
