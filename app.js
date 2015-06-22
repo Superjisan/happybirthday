b7b575edfdf7f046f609f679456a51e2685269b3
Jisan Zaman <jzthegreat@gmail.com>
2015-06-22 13:28:42 -0400

    commented out local server start

--- app.js
+++ app.js
@@ -59,9 +59,9 @@ app.use(function(err, req, res, next) {
 app.set('port', process.env.PORT || 3000);
 
 if(app.get('env') === 'development'){
-  app.listen(app.get('port'), function() {
-    console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
-  });
+  // app.listen(app.get('port'), function() {
+  //   console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
+  // });
     
 }
 
