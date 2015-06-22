2c7dbbfb8bff3aa51c407f877631c0c8385d2a3e
Jisan Zaman <jzthegreat@gmail.com>
2015-06-22 13:40:09 -0400

    properly implemented local machine server bootup, fixed input on home page

--- app.js
+++ app.js
@@ -58,11 +58,10 @@ app.use(function(err, req, res, next) {
 
 app.set('port', process.env.PORT || 3000);
 
-if(app.get('env') === 'development'){
-  // app.listen(app.get('port'), function() {
-  //   console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
-  // });
-    
+if (process.env.NODE_ENV == "development") {
+  app.listen(app.get('port'), function() {
+    console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
+  });
 }
 
 module.exports = app;
